22c066663d4c33804508b4c109d3976b5363ac91
ana-macias <amaciaselopez@gmail.com>
2025-04-26 22:25:51 +0200

    ejercicios

---
+++ 1-Fundamentals/Python/4-Funciones/Practica/funciones.py
@@ -0,0 +1,12 @@
+# Area de un cuadrado
+def area_cuadrado (lado):
+    return lado ** 2
+
+# Area de un triangulo
+def area_triangulo (base, altura):
+    return (base * altura) / 2
+
+# Area de un circulo
+def area_circulo (pi, radio):
+    pi = 3.1416
+    return 3.1416 * radio **2
