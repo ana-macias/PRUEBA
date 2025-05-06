62f1638c32cbd6ef3ebf76c949234c3199405339
ana-macias <amaciaselopez@gmail.com>
2025-05-06 16:48:15 +0200

    intento

--- Hundir_la_flota/main.py
+++ Hundir_la_flota/main.py
@@ -1,8 +1,8 @@
 import utils
 import barcos
 
+tablero_jugador = utils.crear_tablero ()
 
-tablero = utils.crear_tablero()
-print (tablero)
+print (tablero_jugador)
 print ("______________________")
-tablero_modificado = utils.colocar_barco (tablero) and barcos.barco_jugador
+tablero_modificado = utils.colocar_barco (tablero_jugador) and barcos.barco_jugador
--- Hundir_la_flota/utils.py
+++ Hundir_la_flota/utils.py
@@ -1,11 +1,14 @@
 import numpy as np
+import barcos
 
-def crear_tablero():
+
+
+def colocar_barcos(lista_barcos):    
     tablero = np.full ((10,10), "_")
-    return (tablero)
+    tablero_jugador = tablero
 
-def colocar_barco(tablero,barco):
-    for i in barco:
-        tablero [i[0],i[1]] = "O"
-    
+    for barco in lista_barcos:
+        for i, j in barco:
+            tablero_jugador[i, j] = 'O'
+
     return tablero
