274c050caebc6ab0e6aafe8253ddd6b90d3a183e
ana-macias <amaciaselopez@gmail.com>
2025-05-06 13:51:44 +0200

    Intentos

--- Hundir_la_flota/main.py
+++ Hundir_la_flota/main.py
@@ -1,10 +1,8 @@
 import utils
+import barcos
 
-barco_1 = [[0,3],[0,4],[0,5],[0,6]]
 
 tablero = utils.crear_tablero()
 print (tablero)
 print ("______________________")
-tablero_modificado = utils.colocar_barco (tablero,barco_1)
-
-print (tablero_modificado)
+tablero_modificado = utils.colocar_barco (tablero) and barcos.barco_jugador
